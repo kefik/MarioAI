fb60dde9c8596e12bd4b1c0fc759fa1e080aacec
Julius Flimmel <flimmelj@gmail.com>
2019-08-10 14:26:07 +0200

    Removed unnecesary projects

--- MarioAI4J-Playground/compile.bat
+++
@@ -1,6 +0,0 @@
-cd bin
-rmdir /s /q ch
-cd ..
-dir /s /B *.java > sources.txt
-javac -d bin -cp bin;../MarioAI4J/bin;../MarioAI4J/lib/simple-logging-1.0.1.jar;../MarioAI4J-Tournament/bin;../MarioAI4J-Tournament/lib/*;lib/* @sources.txt
-del /Q sources.txt
