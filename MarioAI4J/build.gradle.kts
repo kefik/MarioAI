b810fafa401977039f72955d1b97823e268a5d2b
Julius Flimmel <flimmelj@gmail.com>
2019-09-16 13:45:07 +0200

    Fix gradle build of MarioAI4J project

--- MarioAI4J/build.gradle.kts
+++ MarioAI4J/build.gradle.kts
@@ -26,3 +26,17 @@ val jar by tasks.getting(Jar::class) {
     from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
 
 }
+
+/**
+ * We need this task, because the resources are by default moved directly to build/resources, not their corresponding
+ * folders.
+ */
+tasks.register("fixResources", Copy::class) {
+    from("src/ch/idsia/benchmark/mario/engine/resources")
+    into("$buildDir/resources/main/ch/idsia/benchmark/mario/engine/resources")
+}
+
+tasks.named("processResources", ProcessResources::class) {
+    dependsOn("fixResources")
+}
+
